586f8c8c9c5692f24e2239ddc511b4bbf1e3a1a1
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-10 19:19:55 +0100

    Add tables to seed and use find_or_create_by
    This way if the employees already exist it won't create duplicates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Employee.create(name: 'Dave')
-Employee.create(name: 'Emily')
-Employee.create(name: 'Rashida')
+Employee.find_or_create_by(name: 'Dave')
+Employee.find_or_create_by(name: 'Emily')
+Employee.find_or_create_by(name: 'Rashida')
+
+Table.find_or_create_by(number: 1, size: 2)
+Table.find_or_create_by(number: 2, size: 4)
+Table.find_or_create_by(number: 3, size: 6)
+Table.find_or_create_by(number: 4, size: 2)
+Table.find_or_create_by(number: 5, size: 4)
+Table.find_or_create_by(number: 6, size: 6)
+Table.find_or_create_by(number: 7, size: 2)
+Table.find_or_create_by(number: 8, size: 4)
