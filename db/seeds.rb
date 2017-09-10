32423473f4d1a40bec4f9e1e79fd48b37062f379
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-11 00:14:11 +0100

    Create order seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,14 @@ Meal.find_or_create_by(name: 'burger and fries', category: 'main', cost_price: 3
 Meal.find_or_create_by(name: 'steak and fries', category: 'main', cost_price: 8, sale_price: 20)
 Meal.find_or_create_by(name: 'chocolate cake', category: 'dessert', cost_price: 1, sale_price: 4)
 Meal.find_or_create_by(name: 'ice cream', category: 'dessert', cost_price: 0.5, sale_price: 3)
+
+Order.find_or_create_by(number: 1, employee: Employee.first, table: Table.first)
+Order.find_or_create_by(number: 2, employee: Employee.last, table: Table.last)
+
+OrderMeal.find_or_create_by(order: Order.first, meal: Meal.find(1))
+OrderMeal.find_or_create_by(order: Order.first, meal: Meal.find(4))
+OrderMeal.find_or_create_by(order: Order.first, meal: Meal.find(6))
+
+OrderMeal.find_or_create_by(order: Order.last, meal: Meal.find(2))
+OrderMeal.find_or_create_by(order: Order.last, meal: Meal.find(5))
+OrderMeal.find_or_create_by(order: Order.last, meal: Meal.find(7))
