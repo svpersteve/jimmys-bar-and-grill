0e1558a25c95cb9b430560c2e567713eca705115
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-10 23:53:49 +0100

    Create meal category scopes and partials

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,5 @@
 class Meal < ActiveRecord::Base
+  scope :starters, -> { where(category: 'starter') }
+  scope :mains, -> { where(category: 'main') }
+  scope :desserts, -> { where(category: 'dessert') }
 end
